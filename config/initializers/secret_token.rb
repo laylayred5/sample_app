bd3f8e5edc068ee48e10b838b130c7a4ffd55fd1
Lay lay <laylaymyint@red5studios.com.sg>
2013-04-24 15:30:45 +0800

    Add a StaticPages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a51a54cecba3cceeb2107f75f53653e466b18bed58a55aa9e67238680694d6b47d6c385223e2cddb2d4cc9edec3142d39f1552bc9939f9dc081858fb5cd4f9f7'
